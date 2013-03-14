871ac72d2479e1515cd1f5e654d59e9cf5fc2ef6
Antoine Pietri <antoine.pietri@epita.fr>
2013-03-14 16:21:19 +0000

    partie_tour → jouer_tour

--- src/rules.cc
+++ src/rules.cc
@@ -1,11 +1,55 @@
+#include <fstream>
+
+#include <utils/log.hh>
+#include <rules/player.hh>
+
 #include "rules.hh"
+#include "game.hh"
+#include "map.hh"
+#include "api.hh"
 
 Rules::Rules(const rules::Options opt)
+    : opt_(opt)
 {
     if (!opt.champion_lib.empty())
         champion_dll_ = new utils::DLL(opt.champion_lib);
 
-    // FIXME
+    std::ifstream ifs(opt.map_file);
+    Map* map = new Map;
+    map->load(ifs);
+
+    GameState* game_state = new GameState(map, opt.players);
+    game_state->init();
+
+    int equipe = 0;
+    if (opt.player.get() != nullptr)
+    {
+        for (; (unsigned)equipe < opt.players->players.size(); ++equipe)
+            if (opt.players->players[equipe]->id == opt.player->id)
+                break;
+    }
+
+    api_ = new Api(game_state, opt.player, equipe);
+
+    // Get the champion library if we are a client
+    if (!opt.champion_lib.empty())
+    {
+        champion_ = new utils::DLL(opt.champion_lib);
+        champion_partie_init = champion_->get<f_champion_partie_init>("partie_init");
+        champion_jouer_placement = champion_->get<f_champion_jouer_placement>("jouer_placement");
+        champion_jouer_deplacement = champion_->get<f_champion_jouer_deplacement>("jouer_deplacement");
+        champion_jouer_attaque = champion_->get<f_champion_jouer_attaque>("jouer_attaque");
+        champion_partie_fin = champion_->get<f_champion_partie_fin>("partie_fin");
+
+        if (opt.player->type == rules::SPECTATOR)
+            champion_partie_init();
+        else
+            sandbox_.execute(champion_partie_init);
+    }
+
+    players_ = opt.players;
+    spectators_ = opt.spectators;
+    timeout_ = opt.time;
 }
 
 Rules::~Rules()
--- src/rules.hh
+++ src/rules.hh
@@ -1,29 +1,35 @@
 #ifndef RULES_RULES_HH_
-# define RULES_RULES_HH_
+#define RULES_RULES_HH_
 
-# include <utils/dll.hh>
-# include <utils/sandbox.hh>
-# include <rules/options.hh>
-# include <rules/client-messenger.hh>
-# include <rules/server-messenger.hh>
-# include <rules/player.hh>
+#include <utils/dll.hh>
+#include <utils/sandbox.hh>
+#include <rules/options.hh>
+#include <rules/client-messenger.hh>
+#include <rules/server-messenger.hh>
+#include <rules/player.hh>
 
+#include "api.hh"
 
 class Rules
 {
-public:
-    explicit Rules(const rules::Options opt);
-    virtual ~Rules();
+    public:
+        explicit Rules(const rules::Options opt);
+        virtual ~Rules();
 
-    // FIXME
-    void client_loop(rules::ClientMessenger_sptr msgr);
-    void spectator_loop(rules::ClientMessenger_sptr msgr);
-    void server_loop(rules::ServerMessenger_sptr msgr);
+        // FIXME
+        void client_loop(rules::ClientMessenger_sptr msgr);
+        void spectator_loop(rules::ClientMessenger_sptr msgr);
+        void server_loop(rules::ServerMessenger_sptr msgr);
 
-private:
-    utils::DLL* champion_dll_;
+    protected:
+        f_champion_partie_init champion_partie_init;
+        f_champion_partie_tour champion_partie_tour;
+        f_champion_partie_fin champion_partie_fin;
+    private:
+        const rules::Options opt_;
 
-    // FIXME
+        utils::DLL* champion_;
+        Api* api_;
 };
 
 #endif // !RULES_RULES_HH_
